cb3a3b22cfd8c22f1493eb45ba18b4966ec2788f
Wu <2321141691@qq.com>
2023-04-25 11:46:50 +0800

    测试api

--- test/test.py
+++ test/test.py
@@ -3,7 +3,7 @@ import json
 
 session = requests.session()
 
-url = "http://127.0.0.1:2023/connect"
+url = "http://127.0.0.1:2023/common/connect"
 with open('config.json', 'r') as fp:
     data = json.load(fp=fp)
 json_data = json.dumps(data)  # 将数据编码为JSON格式
@@ -12,10 +12,10 @@ response = session.post(url, json=data)
 print(response.cookies)
 
 
-url = "http://127.0.0.1:2023/test"
+url = "http://127.0.0.1:2023/common/test"
 response = session.get(url)
 print(response.text)
 
-url = "http://127.0.0.1:2023/close"
+url = "http://127.0.0.1:2023/common/close"
 response = session.get(url)
 print(response.text)
