97c46a1a548c0e732b40c9ad7d35403edeff6c89
Wu <2321141691@qq.com>
2023-04-25 21:38:45 +0800

    完善__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,19 @@
 from flask import Flask
-from flask_redis import FlaskRedis
 from instance import redis_conf
 from flask_session import Session
 from redis import Redis
 
 app = Flask(__name__)
 app.secret_key = 'my_secret_key'
-app.config['REDIS_URL'] = redis_conf.redis_url
-redis_client = FlaskRedis(app)
+redis_client = Redis(host=redis_conf.redis_host,
+                     port=redis_conf.redis_port,
+                     password=redis_conf.redis_password,
+                     db=redis_conf.redis_db)
 app.config['SESSION_TYPE'] = 'redis'
-app.config['SESSION_REDIS'] = Redis(host=redis_conf.redis_host,
-                                    port=redis_conf.redis_port,
-                                    password=redis_conf.redis_password,
-                                    db=redis_conf.redis_db)
+app.config['SESSION_REDIS'] = redis_client
 Session(app)
 
-from app import common
+from app import common, openai
 
 app.register_blueprint(common.bp)
+app.register_blueprint(openai.bp)
