359fe23fd208ffa995e9aa01f6e05c487a1a7936
Wu <2321141691@qq.com>
2023-04-25 21:38:22 +0800

    完善ApiBuilder类

--- lib/ApiBuilder.py
+++ lib/ApiBuilder.py
@@ -1,15 +1,25 @@
+import threading
+
+import openai
+
+lock = threading.Lock
+
+
 class ApiBuilder:
 
     @staticmethod
-    def ChatCompletion(openai, msg, chatCompletionConfig):
+    def ChatCompletion(openai_key, msg, chatCompletionConfig):
+        openai.api_key = openai_key
         return openai.ChatCompletion.create(
             messages=msg,
             **vars(chatCompletionConfig)
         )
 
     @staticmethod
-    def Image(openai, prompt, imageConfig):
-        return openai.Image.create(
-            prompt=prompt,
-            **vars(imageConfig)
-        )
+    def Image(openai_key, prompt, imageConfig):
+        with lock:
+            openai.api_key = openai_key
+            return openai.Image.create(
+                prompt=prompt,
+                **vars(imageConfig)
+            )
